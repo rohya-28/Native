1a97267af913c11326bcdbd6434c8a66b3cf83a3
rohya-28 <rohitarsude28@gmail.com>
2022-06-23 00:17:35 -0400

    dep

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import {  FlatList, SafeAreaView, View, Text } from 'react-native'
+
+const Home = () => {
+  return (
+   <Text>Home</Text>
+  )
+}
+
+export default Home
